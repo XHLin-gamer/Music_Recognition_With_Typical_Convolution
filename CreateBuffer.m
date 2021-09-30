033e75ed177931445f81d089a2c9e35ea83feda3
XHLin-gamer <73104227+XHLin-gamer@users.noreply.github.com>
2021-09-30 20:40:36 +0800

    basic version
    包含采样,并行,GPU加速等功能的一个基础版本

---
+++ CreateBuffer.m
@@ -0,0 +1,35 @@
+function [] = CreateBuffer()
+    audioInDir = dir('Data/Music/*.mp4');
+    audioNames = {audioInDir.name};
+
+%     if(length(audioNames)==0)
+%         disp 'No Audio Files in Current Directory'
+%     else
+%         for i=1:length(audioNames)
+%             
+%             filename = char(audioNames(i));
+%             filenameToSave = strcat( '.\Data\Raw\',filename(1:(length(filename)-4)),'.mat');
+%             filename = strcat('Data/Music/',filename);
+%             disp (filename);
+%             Buffer = audioread(filename);
+%             save(filenameToSave,'Buffer');
+%         end
+%     end
+    BuffInDir = dir('Data/Raw/*.mat');
+    BuffName = {BuffInDir.name};
+    if(length(BuffName)~=0)
+         for i=1:length(BuffName)
+            disp (BuffName(i));
+            name = BuffName{i};
+            file = strcat('Data\Raw\',name);
+            y = load(file);
+            sz = size(y.Buffer);
+            process = y.Buffer(1:50:sz(1,1),1);
+            filenameToSave = strcat( '.\Data\Process\',name(1:(length(name)-4)),'.mat');
+            disp (filenameToSave);
+            save(filenameToSave,'process');
+        end
+    end
+    
+    
+end
---
+++ test.m
@@ -0,0 +1,23 @@
+function test(rounds)
+    files = dir('Data/Raw/*.mat');
+    files_names = {files.name};
+    corr = 0;
+    for i = 1:rounds
+        rnd = unidrnd(39);
+        %testTone = getTestTone();
+        targetName = strcat('Data/Raw/',files_names{rnd});
+        targetFile = load(targetName);
+        len = length(targetFile.Buffer) - 441000;
+        rnd_sec = unidrnd(len);
+        end_sec = rnd_sec + 441000;
+        tone = targetFile.Buffer(rnd_sec:1:end_sec);
+        disp(files_names{rnd});
+        %sound(tone,44100);
+        ans = testResult(tone);
+        disp(ans);
+        if(ans == files_names{rnd})
+            corr = corr + 1;
+        end
+    end
+        disp(corr);
+end
